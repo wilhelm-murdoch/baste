9dbc3cf47814c5189afa24facafe47da5aabd256
Wilhelm Murdoch <wilhelm@devilmayco.de>
2021-10-22 23:14:19 +1000

    feature complete

--- src/lib/requires_function.sh
+++ src/lib/requires_function.sh
@@ -1,3 +1,3 @@
 requires() {
-  command -v "${1}" &> /dev/null || err "baste requires '${1}'"
+  command -v "${1}" &> /dev/null || err "baste requires '${1}'; exiting ..."
 }
--- src/root_command.sh
+++ src/root_command.sh
@@ -2,26 +2,48 @@ requires curl
 requires git
 requires jq
 
-token="${GITHUB_TOKEN}"
-if [[ ! "${GITHUB_TOKEN}" ]]; then
-  token="${args[token]}"
+if [[ "${OSTYPE}" == "darwin"* ]]; then
+  requires pbpaste
+else
+  requires xclip
 fi
 
-if [[ ! "${token}" ]]; then
+: "${GITHUB_TOKEN:=}"
+if [[ "${args[--token]}" ]]; then               # First, --token flag takes precedence.
+  token="${args[--token]}"
+elif [[ "${GITHUB_TOKEN}" ]]; then              # Next, we try an environmental variable.
+  token="${GITHUB_TOKEN}"
+elif git config github.token &> /dev/null; then # finally, we fall back to any possible git config.
   token=$(git config github.token)
+else
+  err 'you must specifiy a valid token before posting; exiting ...'
 fi
 
-if [[ ! "${token}" ]]; then
-  err 'you must specifiy a valid token by defining a GITHUB_TOKEN env, using the --token flag or setting it in your global git config.'
+content="${args[--content]}"
+if [[ "${args[--file]}" ]]; then
+  if ! test -f "${args[--file]}"; then
+    err "file ${args[--file]} does not exist; exiting ..."
+  fi
+  content=$(<"${args[--file]}")
+ elif [[ "${args[--clipboard]}" ]]; then
+  [[ "${OSTYPE}" == "darwin"* ]] && content=$(pbpaste) || content=$(xclip -o -selection clipboard)
 fi
+
+if read -t 0; then
+  read -r content
+fi
+
+[[ ! "${content}" ]] && err 'you must provide some kind of content to post; exiting ...'
+
+[[ "${args[--public]}" ]] && public="true" || public="false"
 
 payload=$(
   jq -n \
-    --arg description "description" \
-    --arg name        "${args[--name]}"   \
-    --arg extension   "${args[--extension]}" \
-    --arg content     "derp"        \
-    --arg public      "true"       \
+    --arg     description "${args[--description]}" \
+    --arg     name        "${args[--name]}"        \
+    --arg     extension   "${args[--extension]}"   \
+    --arg     content     "${content}"             \
+    --argjson public      ${public}                \
     '{
       description: $description, 
       public: $public,
@@ -33,8 +55,19 @@ payload=$(
     }'
 )
 
-curl -s -X POST https://api.github.com/gists \
+response=$(
+  curl -s -X POST https://api.github.com/gists \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token ${token}" \
-  ${CURL_OPTS} \
-  -d "${payload}" | jq -r '.html_url'
+  -d "${payload}"
+)
+
+url=$(echo "${response}" | jq -r '.html_url')
+[[ "${url}" == 'null' ]] && err "$(echo "${response}" | jq -r '.message' | tr -d '\n\t')"
+
+if [[ "${args[--skip-short-url]}" ]]; then
+  echo "${url}"
+  exit 0
+fi
+
+curl -is https://git.io -F "url=${url}" | awk -v FS=": " '/^Location/{print $2}'
